ff0b22f7e2cf9258cf42659a7d9d4fb844b52421
Filter363 <90304026+Filter363@users.noreply.github.com>
2021-09-09 16:23:11 +0530

    Update reply.py

--- userplugins/reply.py
+++ userplugins/reply.py
@@ -1,6 +1,6 @@
 """
 VC Music Player, Telegram Voice Chat Userbot
-Copyright (C) 2021  ZauteKm <https://telegram.dog/ZauteKm>
+Copyright (C) 2021  Aditya
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
